8a7aaf69840c427b5c063cb6deff42f1fad917d7
jaikko <65661383+jaikko@users.noreply.github.com>
2021-06-29 21:46:23 +0200

    bug fixed

--- server.py
+++ server.py
@@ -48,6 +48,9 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+    if placesRequired > 12:
+        flash("You can't reserve more than 12 places")
+        return render_template('booking.html', club=club, competition=competition)
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
